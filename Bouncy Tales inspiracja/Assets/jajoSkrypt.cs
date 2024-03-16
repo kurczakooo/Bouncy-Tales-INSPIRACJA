f98306f5ade83cf4b834df2cc96e65e62019a941
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-03-16 14:34:32 +0100

    test change

--- Bouncy Tales inspiracja/Assets/jajoSkrypt.cs
+++ Bouncy Tales inspiracja/Assets/jajoSkrypt.cs
@@ -2,25 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JajoSkrypt : MonoBehaviour
+public class JajoScript : MonoBehaviour
 {
     public Rigidbody2D body;
     public float moveSpeed = 5f;
+    public float jumpHeight = 5f;
+    public Transform groundCheck; // Uziemienie
+    public LayerMask groundLayer; // Warstwa uziemienia
+
+    private bool isGrounded;
 
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        //body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        CheckGrounded();
         MoveBall();
+        Jump();
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
     void MoveBall()
     {
-        float horizontalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(horizontalInput, 0f);
-        body.velocity = new Vector2(body.velocity.x, movement.x * moveSpeed);
+        body.velocity = new Vector2(movement.x * moveSpeed, body.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("SKACZE");
+            body.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
     }
 }
