b7b8f288c827157f0aaddd716415b4f476ea82e0
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-05-08 11:59:13 +0200

    added pause screen  and feature, need to fix health reastarting bug

---
+++ Bouncy Tales inspiracja/Assets/Assets/Wlasne/Player/GUI/pause.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FreezeSceneGUIButton : MonoBehaviour
+{
+    // Funkcja do prze??czania stanu zamro?enia sceny
+    public void ToggleFreezeScene()
+    {
+        // Sprawd?, czy skala czasu jest obecnie zamro?ona
+        if (Time.timeScale == 0f)
+        {
+            // Je?li zamro?one, ustaw skal? czasu z powrotem na normaln? (1)
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            // Je?li nie zamro?one, zamro? scen? ustawiaj?c skal? czasu na 0
+            Time.timeScale = 0f;
+        }
+    }
+}
